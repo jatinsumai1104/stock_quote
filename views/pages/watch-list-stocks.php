797d3db1eb99bb6f83f5339df51d65a63ee742db
nikhil-ghind <nikhilghind29@gmail.com>
2020-02-02 00:27:09 +0530

    set

--- views/pages/watch-list-stocks.php
+++ views/pages/watch-list-stocks.php
@@ -53,7 +53,6 @@
             <!-- Earnings (Monthly) Card Example -->
             
             <h1 class="h3 mb-0 text-gray-800">Watch List</h1>
-            <a href="#" class="d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-plus fa-sm text-white-50"></i> Add Stocks Into Watch List</a>
           </div>
           
           <?php 
