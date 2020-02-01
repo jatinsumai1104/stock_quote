445276522f9286eeb1e4d50a6efa5c43ad8ad4ec
nikhil-ghind <nikhilghind29@gmail.com>
2020-02-02 04:41:57 +0530

    chutiya code

--- helper/constants.php
+++ helper/constants.php
@@ -1,7 +1,7 @@
 <?php
 require_once("status-constants.php");
 
-define("BASEURL", "http://localhost/stock_quote_jatin/");
+define("BASEURL", "http://localhost/stock_quote/");
 define("BASEASSETS", BASEURL . "assets/");
 define("BASEPAGES", BASEURL . "views/pages/");
 
