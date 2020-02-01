8b7864dda3ba29a36c97b6d21d5e2b88da6ef73f
jatinsumai1104 <jatinsumai1104@gmail.com>
2020-02-02 01:53:51 +0530

    bug fix

--- views/pages/index.php
+++ views/pages/index.php
@@ -56,7 +56,7 @@
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                       <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Current Balance</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$<?php  echo $res[0]['balance']?></div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800">$100</div>
                     </div>
                     <div class="col-auto">
                       <i class="fas fa-calendar fa-2x text-gray-300"></i>
